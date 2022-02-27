d74c1f11e4125146fa9d349bb59a5f9433edc2f7
Petr <povar_cr@mail.ru>
2022-02-27 21:39:08 +0300

    add Task_50

---
+++ Task_50/Program.cs
@@ -0,0 +1,56 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные.
+
+
+int[,] array = new int[5, 5];
+Random rnd = new Random();
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(-100, 100);
+        }
+
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+void ChangeEvenNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] % 2 == 0) array[i, j] = -array[i, j];
+        }
+    }
+
+}
+void PrintArray2(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+CreateArray(array);
+Console.WriteLine("Cлучайнный Массив:");
+PrintArray(array);
+Console.WriteLine("Массив после замены четных элементов на противоположные");
+ChangeEvenNumbers(array);
+PrintArray2(array);
+
